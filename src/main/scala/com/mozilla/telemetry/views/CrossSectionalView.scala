bed28a6ecd4fc86addbecef05ec9facd2aa238e4
Ryan Harter <harterrt@gmail.com>
2016-08-10 11:28:42 -0700

    Format sample xsec file

--- src/main/scala/com/mozilla/telemetry/views/CrossSectionalView.scala
+++ src/main/scala/com/mozilla/telemetry/views/CrossSectionalView.scala
@@ -1,21 +1,16 @@
+package com.mozilla.telemetry.views
+
 import org.apache.spark.sql.SparkSession
 
 val spark = SparkSession.builder().appName("Cross Sectional Example").getOrCreate()
 
-// For implicit conversions like converting RDDs to DataFrames
 import spark.implicits._
 
-val ll = spark.read.load("/home/harterrt/data/l10l_20160725_single_shard.parquet")
-
 case class longitudinal (
-  client_id: String
+    client_id: String
   , geo_country: Seq[String]
   , session_length: Seq[Long]
 )
-
-val ds = ll.as[longitudinal]
-val row = ds.take(1)
-val elem = row(0)
 
 def weightedMode[T <: Comparable[T]](values: Seq[T], weights: Seq[Long]) = {
   val pairs = values zip weights
@@ -26,3 +21,13 @@ def weightedMode[T <: Comparable[T]](values: Seq[T], weights: Seq[Long]) = {
 def modalCountry(row: longitudinal) = {
   weightedMode(row.geo_country, row.session_length)
 } 
+
+def getData() = {
+  val ll = spark.read.load("/home/harterrt/data/l10l_20160725_single_shard.parquet")
+
+  val ds = ll.as[longitudinal]
+  val row = ds.take(1)
+  val elem = row(0)
+  
+  (ll, ds, row, elem)
+}
