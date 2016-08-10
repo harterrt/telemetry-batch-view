0cc34549b96e8a631368fa76a255046013d6f3c8
Ryan Harter <harterrt@gmail.com>
2016-08-09 17:27:16 -0700

    Adds proof of concept CrossSectional code

---
+++ src/main/scala/com/mozilla/telemetry/views/CrossSectionalView.scala
@@ -0,0 +1,28 @@
+import org.apache.spark.sql.SparkSession
+
+val spark = SparkSession.builder().appName("Cross Sectional Example").getOrCreate()
+
+// For implicit conversions like converting RDDs to DataFrames
+import spark.implicits._
+
+val ll = spark.read.load("/home/harterrt/data/l10l_20160725_single_shard.parquet")
+
+case class longitudinal (
+  client_id: String
+  , geo_country: Seq[String]
+  , session_length: Seq[Long]
+)
+
+val ds = ll.as[longitudinal]
+val row = ds.take(1)
+val elem = row(0)
+
+def weightedMode[T <: Comparable[T]](values: Seq[T], weights: Seq[Long]) = {
+  val pairs = values zip weights
+  val agg = pairs.groupBy(_._1).map(kv => (kv._1, kv._2.map(_._2).sum))
+  agg.maxBy(_._2)._1
+}
+
+def modalCountry(row: longitudinal) = {
+  weightedMode(row.geo_country, row.session_length)
+} 
