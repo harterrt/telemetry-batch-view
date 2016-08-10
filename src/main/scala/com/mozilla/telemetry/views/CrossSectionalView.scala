1cb928d72c2815b4db618e0fb6601a14483f982e
Ryan Harter <harterrt@gmail.com>
2016-08-10 15:36:56 -0700

    Convert SparkSession to SQLContext
    Moving the repo to Spark 2.0 is out of scope for this CL

--- src/main/scala/com/mozilla/telemetry/views/CrossSectionalView.scala
+++ src/main/scala/com/mozilla/telemetry/views/CrossSectionalView.scala
@@ -1,10 +1,9 @@
 package com.mozilla.telemetry.views
 
-import org.apache.spark.sql.SparkSession
 
-val spark = SparkSession.builder().appName("Cross Sectional Example").getOrCreate()
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SQLContext
 
-import spark.implicits._
 
 case class longitudinal (
     client_id: String
@@ -12,22 +11,31 @@ case class longitudinal (
   , session_length: Seq[Long]
 )
 
-def weightedMode[T <: Comparable[T]](values: Seq[T], weights: Seq[Long]) = {
-  val pairs = values zip weights
-  val agg = pairs.groupBy(_._1).map(kv => (kv._1, kv._2.map(_._2).sum))
-  agg.maxBy(_._2)._1
-}
+object CrossSectionalView {
+  val sparkConf = new SparkConf().setAppName("Cross Sectional Example")
+  sparkConf.setMaster(sparkConf.get("spark.master", "local[*]"))
+  implicit val sc = new SparkContext(sparkConf)
+  val sqlContext = new SQLContext(sc)
 
-def modalCountry(row: longitudinal) = {
-  weightedMode(row.geo_country, row.session_length)
-} 
+  import sqlContext.implicits._
 
-def getData() = {
-  val ll = spark.read.load("/home/harterrt/data/l10l_20160725_single_shard.parquet")
+  def weightedMode[T <: Comparable[T]](values: Seq[T], weights: Seq[Long]) = {
+    val pairs = values zip weights
+    val agg = pairs.groupBy(_._1).map(kv => (kv._1, kv._2.map(_._2).sum))
+    agg.maxBy(_._2)._1
+  }
 
-  val ds = ll.as[longitudinal]
-  val row = ds.take(1)
-  val elem = row(0)
-  
-  (ll, ds, row, elem)
+  def modalCountry(row: longitudinal) = {
+    weightedMode(row.geo_country, row.session_length)
+  } 
+
+  def getData() = {
+    val ll = sqlContext.read.load("/home/harterrt/data/l10l_20160725_single_shard.parquet")
+
+    val ds = ll.as[longitudinal]
+    val row = ds.take(1)
+    val elem = row(0)
+    
+    (ll, ds, row, elem)
+  }
 }
