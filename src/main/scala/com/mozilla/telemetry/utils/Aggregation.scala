e54d0e6c6f61912aaf687adc673a303ed3df49dc
Ryan Harter <harterrt@gmail.com>
2016-10-04 17:36:54 -0400

    Add remaining columns to cross sectional database
    This commit gives us feature parity with the python prototype by
    bcolloran:
    https://github.com/mozilla/bcolloran_spark/blob/master/perClientDataPrep/cross-sectional%20data%20table%20%282016-07-14%29%20-%20deduped%20clientIds.ipynb

--- src/main/scala/com/mozilla/telemetry/utils/Aggregation.scala
+++ src/main/scala/com/mozilla/telemetry/utils/Aggregation.scala
@@ -1,24 +1,42 @@
 package com.mozilla.telemetry.utils
 
 package object aggregation {
-  def mean(values: Seq[Int]) = {
-    if (values.length > 0) {
-      Some(values.sum.toDouble / values.length)
+  private def validateWeights[A](values: Seq[A], weights: Seq[Long]) = {
+    if (values.size != weights.size) {
+      throw new IllegalArgumentException("Weights and values must have the same length.")
+    }
+    if (values.size == 0) {
+      throw new IllegalArgumentException("Weights and values must have length > 0.")
+    }
+    if (weights.sum <= 0) {
+      throw new IllegalArgumentException("Weights must have positive sum")
+    }
+
+    true
+  }
+
+  def mean(values: Seq[Long]) = {
+    if (values.size > 0) {
+      Some(values.sum.toDouble / values.size)
     } else {
       None
     }
   }
 
+  def weightedMean(values: Seq[Option[Long]], weights: Seq[Long]): Double = {
+    validateWeights(values, weights)
+    val clean_pairs = (values zip weights).map(x => x match{
+      case (Some(v), w) => Some((v, w))
+      case _ => None
+    }).flatten
+    clean_pairs.foldLeft(0.0)((acc, pair) => acc + (pair._1 * pair._2)) / weights.sum
+  }
+
   def weightedMode[A](values: Seq[A], weights: Seq[Long]): A = {
-    if (values.size != weights.size) {
-      throw new IllegalArgumentException("Args to weighted mode must have the same length.")
-    } else if (values.size == 0) {
-      throw new IllegalArgumentException("Args to weighted mode must have length > 0.")
-    } else {
-      val pairs = values zip weights
-      val agg = pairs.groupBy(_._1).map(kv => (kv._1, kv._2.map(_._2).sum))
-      agg.maxBy(_._2)._1
-    }
+    validateWeights(values, weights)
+    val pairs = values zip weights
+    val agg = pairs.groupBy(_._1).map(kv => (kv._1, kv._2.map(_._2).sum))
+    agg.maxBy(_._2)._1
   }
 }
 
